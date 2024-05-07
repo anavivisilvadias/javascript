fdaa7b03e018192a734994c0b5887d3e9211db84
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-07 08:46:32 -0400

    adicionar template3

---
+++ template3.js
@@ -0,0 +1,16 @@
+let textoMultilinha = `
+essa é uma string
+que ocupa múltiplas linhas
+e você não precisa se preocupar
+com quebras de linha ou concatenação.
+`;
+console.log(textoMultilinha);
+/*
+Saída:
+"
+Esta é uma string
+que ocupa múltiplas linhas
+e você não precisa se preocupar
+com quebras de linha ou concatenação
+"
+*/

e0a2945b90865a90b97204e716777c25ece7f7be
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-07 08:45:25 -0400

    adicionar template

---
+++ template.js
@@ -0,0 +1,6 @@
+let nome = "Wanderson Timóteo";
+let idade = 30;
+
+// Usando template literals para criar uma string com variáveis
+let saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(saudacao); // Saída: "Oá, meu nome é Wanderson Timóteo e eu tenho 30 anos."

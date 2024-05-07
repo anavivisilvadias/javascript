3303cea8e287c574e9ffc1c8dcbde76c17961d70
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-07 08:40:36 -0400

    exercicio conca2

---
+++ concatenacao2.js
@@ -0,0 +1,4 @@
+let nome = "Ryan";
+let idade = 11;
+let saudacao = "Olá, meu nome é" + nome + "e eu tenho" + idade + "anos.";
+console.log(saudacao); // Saída: "Olá, meu nome é Ryan e eu tenho 11 anos."

0db901dbb10d48547978c236aa23a60560881df0
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-07 08:41:25 -0400

    adicionar conversao

---
+++ conversao.js
@@ -0,0 +1,8 @@
+let concatenacao ="1.1" + "1.1";
+let converterParaNumero = + "1.1";
+console.log(concatenacao); // "1.11.1"
+console.log(typeof(concatenacao)); // string
+console.log("");
+console.log(converterParaNumero); // 1.1 
+console.log(typeof(converterParaNumero)); // number
+    

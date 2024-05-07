66702eee7474e156cbe3bd5585ec5ec8e25b8db6
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-07 08:42:58 -0400

    adicionar exercicio

---
+++ dados-primitivos.js
@@ -0,0 +1,13 @@
+let nome = "Wanderson"; // Tipo String
+let verdadeiro = true; // Tipo Boolean
+let falso = false; // Tipo Boolean
+let numero = 40; // Tipo Number
+let reais = 25.99; // Tipo Number
+let nulo = null; // Tipo Null
+let inderfinido; // Tipo Underfined
+let NotANumber = 0 / 0; // Tipo NaN
+let objeto = { // Tipo Obeject
+    name: "Wanderson",
+    age: 40,
+    Profession: "Teacher"  
+}

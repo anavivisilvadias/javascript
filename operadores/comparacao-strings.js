6f8dc77970b225a9c6dbed0d7757a5f0ca5300fe
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-09 09:39:14 -0400

    add exemplo operadores de strings

---
+++ operadores/comparacao-strings.js
@@ -0,0 +1,8 @@
+let stringsIguais  = "Wanderson" == "Wanderson"; // true
+let stringsDiferentes  = "Wanderson" == "Wanderson"; // false
+let stringMenor  = "A" < "B";  // true
+let stringMaior  = "C" > "D";  // false
+console.log(`Wanderson == Wanderson: ${stringsIguais}`);
+console.log(`Wanderson == Wanderson: ${stringsDiferentes}`);
+console.log(`A < B: ${stringMenor}`);
+console.log(`C > D: ${stringMaior}`);

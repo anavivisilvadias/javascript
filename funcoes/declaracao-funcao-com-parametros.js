dda558cce0467d23b1675081b30bd0276c849098
anavivisilvadias <anavitoriadiasdasilva47@gmail.com>
2024-05-14 11:17:24 -0400

    add exemplo funcao parametro

---
+++ funcoes/declaracao-funcao-com-parametros.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametro1, parametro2) {
+    // Corpo da função
+    // Pode conter uma ou mais instruções
+    return resultado; // Opcional
+}
+// Chamada da função
+nomeDaFuncao(parametro1, parametro2);
